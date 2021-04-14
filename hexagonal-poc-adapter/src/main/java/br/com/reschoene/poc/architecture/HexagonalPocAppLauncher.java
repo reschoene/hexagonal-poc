f4f1a0245b6b4eee5379b840999025cee1ded377
reschoene <reschoene@gmail.com>
2021-04-14 19:10:42 -0300

    reestruturação de pacotes e projetos

---
+++ hexagonal-poc-adapter/src/main/java/br/com/reschoene/poc/architecture/HexagonalPocAppLauncher.java
@@ -0,0 +1,11 @@
+package br.com.reschoene.poc.architecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexagonalPocAppLauncher {
+    public static void main(String[] args) {
+        SpringApplication.run(HexagonalPocAppLauncher.class, args);
+    }
+}
